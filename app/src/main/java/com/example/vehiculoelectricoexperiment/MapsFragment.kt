978230b2a635329c4aca22507fca7c657ff44178
Andres Sanchez Salcedo <andres.sanchezz117@gmail.com>
2022-05-04 17:59:58 -0500

    Add maps functionality for location, routes and search autocomplete

--- app/src/main/java/com/example/vehiculoelectricoexperiment/MapsFragment.kt
+++ app/src/main/java/com/example/vehiculoelectricoexperiment/MapsFragment.kt
@@ -1,11 +1,21 @@
 package com.example.vehiculoelectricoexperiment
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,8 +23,38 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.RoundCap
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.maps.DirectionsApi
+import com.google.maps.GeoApiContext
+import com.google.maps.android.PolyUtil
+import com.google.maps.model.DirectionsResult
 
 class MapsFragment : Fragment() {
+    // Represents the google map itself
+    private lateinit var googleMap: GoogleMap
+
+    // The entry point to the Fused Location Provider.
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var context: GeoApiContext
+
+    // Variable to know if the location premission has been granted
+    private var locationPermissionGranted = false
+
+    // The geographical location where the device is currently located. That is, the last-known
+    // location retrieved by the Fused Location Provider.
+    private var lastKnownLocation: Location? = null
+
+    // A default location CUCEI
+    private val defaultLocation = LatLng(20.65889, -103.32315)
+
+    // The entry point to the Places API.
+    private lateinit var placesClient: PlacesClient
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -26,9 +66,18 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        this.googleMap = googleMap
+
+        // Prompt the user for permission.
+        getLocationPermission()
+
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI()
+
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation("${defaultLocation.latitude},${defaultLocation.longitude}")
+
+        googleMap.addMarker(MarkerOptions().position(defaultLocation).title("CUCEI"))
     }
 
     override fun onCreateView(
@@ -41,7 +90,153 @@ class MapsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Construct a FusedLocationProviderClient.
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.requireActivity())
+        context = GeoApiContext.Builder().apiKey(BuildConfig.MAPS_API_KEY).build()
+
+        // Construct a PlacesClient
+        Places.initialize(this.requireContext(), BuildConfig.MAPS_API_KEY)
+        placesClient = Places.createClient(this.requireContext())
+
+        // Initialize the AutocompleteSupportFragment.
+        val autocompleteFragment =
+            childFragmentManager.findFragmentById(R.id.autocomplete_fragment)
+                    as AutocompleteSupportFragment
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.LAT_LNG))
+
+        // TODO Restrict places search to Guadalajara
+        // Set country
+        autocompleteFragment.setCountries("MX")
+
+        // TODO try to implement a custom button to trigger the search
+        // TODO Maybe also implement a button that locks the camera on the location
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                Log.i("Place selected: ", "Place: ${place.latLng}, ${place.id}")
+                googleMap.clear()
+                googleMap.addMarker(MarkerOptions().position(place.latLng!!).title("Destino"))
+                getDeviceLocation("${place.latLng!!.latitude},${place.latLng!!.longitude}")
+            }
+
+            override fun onError(status: Status) {
+                Log.i("Error with selection: ", "An error occurred: $status")
+            }
+        })
+
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+
+    /**
+     * Prompts the user for permission to use the device location.
+     */
+    private fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(this.requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
+    }
+
+    /**
+     * Handles the result of the request for location permissions.
+     */
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        locationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+        updateLocationUI()
+    }
+
+    /**
+     * Updates the map's UI settings based on whether the user has granted location permission.
+     */
+    @SuppressLint("MissingPermission")
+    private fun updateLocationUI() {
+        try {
+            if (locationPermissionGranted) {
+                googleMap.isMyLocationEnabled = true
+                googleMap.uiSettings.isMyLocationButtonEnabled = false
+            } else {
+                googleMap.isMyLocationEnabled = false
+                googleMap.uiSettings.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    /**
+     * Gets the current location of the device, and positions the map's camera.
+     */
+    @SuppressLint("MissingPermission")
+    private fun getDeviceLocation(destination: String) {
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(this.requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation!!.latitude,
+                                    lastKnownLocation!!.longitude), DEFAULT_ZOOM.toFloat()))
+
+                            val origin = "${lastKnownLocation!!.latitude},${lastKnownLocation!!.longitude}"
+                            //val destination = "20.658423284767768,-103.32246181748931"
+                            drawPolyline(origin, destination)
+                        }
+                    } else {
+                        // Current location is null, use defaults
+                        googleMap.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat()))
+                        googleMap.uiSettings.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    private fun drawPolyline(origin: String, destination: String) {
+        val directionsResult: DirectionsResult = DirectionsApi.getDirections(context, origin, destination).await()
+        val encodedPath: String = directionsResult.routes[0].overviewPolyline.encodedPath
+        val decodedPath: List<LatLng> = PolyUtil.decode(encodedPath)
+        googleMap.addPolyline(
+            PolylineOptions().addAll(decodedPath).color(0xff0080ff.toInt()).startCap(
+                RoundCap()
+            ).endCap(RoundCap()))
+    }
+
+    companion object {
+        private const val DEFAULT_ZOOM = 18
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
     }
 }
